fe13675680ac35305980322fd5c5840adefada2b
Ehsan Samavati <samavati.ehsan@yahoo.com>
2020-12-31 22:17:54 +0000

    babel preset env added

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,17 @@ const config = {
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime']
+                    }
+                }
+            },
+            {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
